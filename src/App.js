d2998c378c0daa6b4f70231f6294523a4aeba9d1
Yoovin <soo22839@gmail.com>
2019-12-30 08:28:11 +0900

    어느정도 완성

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Main from './components/Main'
+// import seafrog from './Img/seafrog.png'
+// import seafrog from './Img/wseafrog.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="out">
+        <div className="in">
+          <Main/>
+          {/* <img className="seafrog" src={seafrog} width="100px" alt="frog"/> */}
+          {/* <span className="nextButton">다음</span> */}
+      </div>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+
+    state = {
+        allTimeLeft:0,
+        allTimePer:0,
+
+    }
+
+    msToTime = (s) =>{
+        let ms = s % 1000;
+        s = (s - ms) / 1000;
+        let secs = s % 60;
+        s = (s - secs) / 60;
+        let mins = s % 60;
+        let days = Math.floor((s - mins) / 1440)
+        let hrs = ((s - mins)/60) - (days * 24);
+
+        let time = {
+            day:days,
+            hour:hrs,
+            minute:mins,
+            second:secs
+        }
+        
+        return time
+    }
+
+    printLeftTime = () => {
+
+        // Time now
+        const thisTime = new Date()
+        const thisSecond = Date.parse(thisTime)
+
+        // Time for soldier
+        const soldierStart = new Date(2020, 0, 6, 14)
+        const soldierEnd = new Date(2021, 8, 13, 8)
+        const soldierStartSecond = Date.parse(soldierStart)
+        const soldierEndSecond = Date.parse(soldierEnd)
+        let leftTime = soldierEndSecond - thisSecond
+
+        // Time for ilgyeong
+        const ilgyeongStart = new Date(2020, 2, 1)
+        const ilgyeongStartSecond = Date.parse(ilgyeongStart)
+        let ilgyeongLeftSecond = ilgyeongStartSecond - thisSecond
+
+        // Time for sanggyeong
+        const sanggyeongStart = new Date(2020, 8, 1)
+        const sanggyeongStartSecond = Date.parse(sanggyeongStart)
+        let sanggyeongLeftSecond = sanggyeongStartSecond - thisSecond
+
+        // Time for sugyeong
+        const sugyeongStart = new Date(2021, 2, 1)
+        const sugyeongStartSecond = Date.parse(sugyeongStart)
+        let sugyeongLeftSecond = sugyeongStartSecond - thisSecond
+
+        // Percent
+        let allPer = ((thisSecond - soldierStartSecond) / (soldierEndSecond - soldierStartSecond)) * 100
+        // let ilPer = ((thisSecond - ilgyeongStartSecond) / (sanggyeongStartSecond - soldierStartSecond)) * 100
+        // let sangPer = ((thisSecond - sanggyeongStartSecond) / (sugyeongStartSecond - sanggyeongStartSecond)) * 100
+        // let suPer = ((thisSecond - sugyeongStartSecond) / (sanggyeongStartSecond - soldierStartSecond)) * 100
+
+        this.setState({
+            allTimeLeft:leftTime,
+            allTimePer:allPer.toFixed(6),
+            ilgyeongLeft: ilgyeongLeftSecond,
+            sanggyeongLeft: sanggyeongLeftSecond,
+            sugyeongLeft: sugyeongLeftSecond,
+            // ilPer:ilPer.toFixed(6),
+            // sangPer:sangPer.toFixed(6),
+            // suPer:suPer.toFixed(6),
+        })
+
+        setTimeout(this.printLeftTime, 1000)
+    }
+
+    componentDidMount(){
+        this.printLeftTime()
+    }
+
+    render() {
+        let {
+            allTimeLeft,
+            allTimePer, 
+            ilgyeongLeft, 
+            sanggyeongLeft, 
+            sugyeongLeft,
+            // ilPer,
+            // sangPer,
+            // suPer
+            } = this.state
+        let {day, hour, minute, second} = this.msToTime(allTimeLeft)
+        let ilgyeong = this.msToTime(ilgyeongLeft)
+        let sanggyeong = this.msToTime(sanggyeongLeft)
+        let sugyeong = this.msToTime(sugyeongLeft)
+        return (
+            <div>
+                <span className="hangleLeftGuide">유빈이의 남은 군생활</span><br/>
+                <span className="time">{day}</span>
+                <span className="hangleTime">일</span><br/>
+                <span className="time">{hour}</span>
+                <span className="hangleTime">시간 </span>
+                <span className="time">{minute}</span>
+                <span className="hangleTime">분 </span>
+                <span className="time">{second}</span>
+                <span className="hangleTime">초</span><br/>
+                <span className="percent">{allTimePer}</span>
+                <span className="hangleTime">%</span><br/><br/>
+                {/* <span>군생활 스타트 : </span>
+                <span>2020</span><span>년</span>
+                <span>1</span><span>월</span>
+                <span>6</span><span>일</span>
+                <span>14</span><span>시</span> */}
+                <span className="solStart">군생활 시작 : 2020년 1월 6일</span><br/>
+                <span className="solStart">군생활 끝 : 2021년 9월 13일</span>
+                <br/><br/>
+                <div className="classIn">
+                    <span className="cUp">일경 진급</span><br/>
+                    <span className="ctime">{ilgyeong.day}</span>
+                    <span className="changleTime">일 </span>
+                    <span className="ctime">{ilgyeong.hour}</span>
+                    <span className="changleTime">시간 </span>
+                    <span className="ctime">{ilgyeong.minute}</span>
+                    <span className="changleTime">분 </span>
+                    <span className="ctime">{ilgyeong.second}</span>
+                    <span className="changleTime">초</span><br/>
+                    {/* <span className="cpercent">{ilPer}</span>
+                    <span className="changleTime">%</span> */}
+                </div><br/>
+                <div className="classIn">
+                    <span className="cUp">상경 진급</span><br/>
+                    <span className="ctime">{sanggyeong.day}</span>
+                    <span className="changleTime">일 </span>
+                    <span className="ctime">{sanggyeong.hour}</span>
+                    <span className="changleTime">시간 </span>
+                    <span className="ctime">{sanggyeong.minute}</span>
+                    <span className="changleTime">분 </span>
+                    <span className="ctime">{sanggyeong.second}</span>
+                    <span className="changleTime">초</span><br/>
+                    {/* <span className="cpercent">{sangPer}</span>
+                    <span className="changleTime">%</span> */}
+                </div><br/>
+                <div className="classIn">
+                    <span className="cUp">수경 진급</span><br/>
+                    <span className="ctime">{sugyeong.day}</span>
+                    <span className="changleTime">일 </span>
+                    <span className="ctime">{sugyeong.hour}</span>
+                    <span className="changleTime">시간 </span>
+                    <span className="ctime">{sugyeong.minute}</span>
+                    <span className="changleTime">분 </span>
+                    <span className="ctime">{sugyeong.second}</span>
+                    <span className="changleTime">초</span><br/>
+                    {/* <span className="cpercent">{suPer}</span>
+                    <span className="changleTime">%</span> */}
+                </div>
+            </div>
+        )
+    }
+}
