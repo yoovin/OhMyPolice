0c7844bbdb5aa4fff89a9a742496ede51945da7d
Yoovin <soo22839@gmail.com>
2019-12-30 08:53:19 +0900

    Updates

---
+++ precache-manifest.048d6b09cd618518fadc79e95eba8fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4a2a89afde0f9b642916d699d4b1f9",
+    "url": "/OhMyPolice/index.html"
+  },
+  {
+    "revision": "2974889dec3afee30b49",
+    "url": "/OhMyPolice/static/css/main.360f52be.chunk.css"
+  },
+  {
+    "revision": "77a98b616d174a962139",
+    "url": "/OhMyPolice/static/js/2.a3c643a0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/OhMyPolice/static/js/2.a3c643a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2974889dec3afee30b49",
+    "url": "/OhMyPolice/static/js/main.eba1b7cf.chunk.js"
+  },
+  {
+    "revision": "572accbe563cf0119788",
+    "url": "/OhMyPolice/static/js/runtime-main.8ad9431d.js"
+  }
+]);
